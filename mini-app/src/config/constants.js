d73b12b8fb2c903db65de875737282a60463c01c
a1ecksa <sashaflejshman123456s@gmail.com>
2026-03-11 23:39:15 +0300

    fast_fix

---
+++ mini-app/src/config/constants.js
@@ -0,0 +1,12 @@
+export const APP_CONFIG = {
+    API_URL: 'http://localhost:8080/api',
+    SUPPORT_URL: 'https://t.me/your_support',
+};
+
+export const MOCK_USER = {
+    daysLeft: 13,
+    trafficUsed: 163577856000, // ~152 ГБ
+    locationsCount: 28,
+    status: 'Premium',
+    untilDate: '2026-03-18'
+};

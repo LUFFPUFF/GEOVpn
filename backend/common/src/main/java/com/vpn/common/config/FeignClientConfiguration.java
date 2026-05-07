13609361da668fea28f0c89794ec3173121109ed
LUFFPUFF <fleyshman_n_e@sc.vsu.ru>
2026-05-07 16:19:08 +0300

    geo 21.09 Исправлен FeignClientConfiguration

--- backend/common/src/main/java/com/vpn/common/config/FeignClientConfiguration.java
+++ backend/common/src/main/java/com/vpn/common/config/FeignClientConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
 /**
  * Конфигурация для Feign клиентов
  */
-@Configuration
 public class FeignClientConfiguration {
 
     @Value("${service.security.internal-secret}")
